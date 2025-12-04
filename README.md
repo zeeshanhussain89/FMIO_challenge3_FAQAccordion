8aabdbc54099d74f75647abec04476b821f32436
zeeshanhussain89 <er.zeeshanhussain@gmail.com>
2025-12-05 01:11:30 +0530

    Solution

---
+++ README.md
@@ -0,0 +1,21 @@
+# Frontend Mentor - FAQ accordion solution
+
+This is a solution to the [FAQ accordion challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz)
+
+### The challenge
+
+Users should be able to:
+
+- Hide/Show the answer to a question when the question is clicked
+- Navigate the questions and hide/show answers using keyboard navigation alone
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![Alt text](screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
---
+++ script.js
@@ -0,0 +1,34 @@
+const questions = document.querySelectorAll('.question');
+const questionsArr = [];
+
+function init() {
+  questions.forEach((question) => {
+    const questionObj = { id: question.getAttribute('id'), state: 'collapsed' };
+    questionsArr.push(questionObj);
+    question.addEventListener('click', toggleState);
+  });
+  //   console.log(questionsArr);
+}
+
+function toggleState(e) {
+  const questionDiv = e.target.parentElement;
+  const question = questionsArr.filter(
+    (q) => q.id === questionDiv.getAttribute('id')
+  );
+  console.log(question[0]);
+  const imgBtn = questionDiv.querySelector('img');
+
+  if (question[0].state === 'collapsed') {
+    imgBtn.setAttribute('src', 'assets/images/icon-minus.svg');
+    question[0].state = 'expanded';
+    const answer = document.getElementById(`answer${question[0].id[8]}`);
+    answer.classList.remove('hide');
+  } else {
+    imgBtn.setAttribute('src', 'assets/images/icon-plus.svg');
+    question[0].state = 'collapsed';
+    const answer = document.getElementById(`answer${question[0].id[8]}`);
+    answer.classList.add('hide');
+  }
+}
+
+init();
